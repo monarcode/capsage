2ef83566a311e3087788d64bbddae4be3d92f073
Ugbah Isioma <61241222+monarcode@users.noreply.github.com>
2021-04-26 16:16:37 +0100

    init

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    const toggler = document.getElementById('toggler');
+    const navlinks = document.querySelector('.navbar__links');
+
+
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop: true,
+        autoplay: true
+    });
+
+    toggler.addEventListener('click', e => {
+        navlinks.classList.toggle('active');
+        // toggler.classList.toggle('vortex');
+    })
+
+    // scroll magic
+    var controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({triggerElement: "#solutions"})
+					.setClassToggle("#navbar", "scroll") // add class toggle
+					.addTo(controller);
+
+
+    AOS.init({
+        duration: 1000,
+        easing: "ease-out",
+    });
+});
