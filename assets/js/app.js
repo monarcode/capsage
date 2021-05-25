802904e806b313dfab2a3143cc6a2cc34b78e1c5
Ugbah Isioma <61241222+monarcode@users.noreply.github.com>
2021-05-25 16:19:31 +0100

    added journey

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,9 @@ $(document).ready(function(){
     new ScrollMagic.Scene({triggerElement: "#portfolio"})
 					.setClassToggle("#navbar", "scroll") // add class toggle
 					.addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#brief"})
+					.setClassToggle("#navbar", "scroll") // add class toggle
+					.addTo(controller);
     AOS.init({
         duration: 1000,
         easing: "ease-out",
